379ffdbca0dc6f5f4a83e27e63befa1328fa4b70
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2016-06-26 11:46:04 +0000

    user: email validate, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
+  
+  # データを保存する直前にemail値を小文字に強制変換
+  before_save { self.email = email.downcase }
+  
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  
+  # セキュアにハッシュ化したパスワードを、データベース内のpassword_digestという属性に保存できるようになる。
+  # 2つのペアの仮想的な属性18 (passwordとpassword_confirmation)が使えるようになる。また、存在性と値が一致するかどうかのバリデーションも追加される。
+  # authenticateメソッドが使えるようになる (引数の文字列がパスワードと一致するとUserオブジェクトを、間違っているとfalse返すメソッド)。
+  has_secure_password
+  
 end
