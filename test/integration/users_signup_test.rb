72883679e27dc6002be9695c0d9635dbef3957e9
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2016-06-27 14:24:34 +0000

    refactoring & test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
   
 end
